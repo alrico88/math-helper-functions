b481ec695f95a16936adab5490e514aabbca8633
carolinatenllado <carolina.tenllado@weplananalytics.com>
2023-11-03 10:31:18 +0100

    fix: percentages

--- src/modules/distributions.ts
+++ src/modules/distributions.ts
@@ -162,7 +162,7 @@ export function calcDistributionWithSeries(
     return {
       name: serieName,
       count: dataArr.map((d) => d.data),
-      percentage_serie: dataArr.map((d) => calcPercent(d.data * 100, serieCount)),
+      percentage_serie: dataArr.map((d) => calcPercent(d.data, serieCount)),
     };
   });
 
@@ -170,7 +170,7 @@ export function calcDistributionWithSeries(
     labels: buckets.map((b) => b.label),
     data: data.map((i) => ({
       ...i,
-      percentage_total: i.count.map((d) => calcPercent(d * 100, totalCount)),
+      percentage_total: i.count.map((d) => calcPercent(d, totalCount)),
     })),
   };
 }
